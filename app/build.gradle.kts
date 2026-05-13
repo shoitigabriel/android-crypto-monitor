8db77b2c85a8bcd247b90795a2e5c24d8f5403b2
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-13 18:32:22 -0300

    second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "shoitigabriel.com.github.cryptomonitor"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "shoitigabriel.com.github.cryptomonitor"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
